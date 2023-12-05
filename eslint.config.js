6dc614da6341268bd638bf749c8fceae97cbb49f
jill64 <intents.turrets0h@icloud.com>
2023-12-05 15:59:15 +0900

    Fix tsConfigPath in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,5 +2,5 @@ import { tsConfig } from '@jill64/eslint-config-ts'
 
 /** @type {import('@jill64/eslint-config-ts').FlatConfig[]} */
 export default tsConfig({
-  tsConfig: './eslint.tsconfig.json'
+  tsConfigPath: './eslint.tsconfig.json'
 })
